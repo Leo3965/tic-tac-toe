275a8f365fa5583ba7e20620f3dd4e5bf42fbaf6
leonardo <leofreitas.engineer@gmail.com>
2023-11-13 14:41:08 -0300

    feat: update player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,30 @@
-export default function Player({name, symbol}) {
+import { useState } from 'react'
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false)
+  const handleButton = () => {
+    setIsEditing(!isEditing)
+  }
+
+  const savePlayerName = (e) => {
+    name = e.target.innerText
+    setIsEditing(false)
+  }
+
+  let playerName = <span className='player-name'>{name}</span>
+  let button = <button onClick={handleButton}>Edit</button>
+  if (isEditing) {
+    playerName = <input type='text' value={name} required />
+    button = <button onClick={handleButton}>Save</button>
+  }
+
   return (
     <li>
             <span className='player'>
-              <span className='player-name'>{name}</span>
+              {playerName}
               <span className='player-symbol'>{symbol}</span>
             </span>
-      <button>Edit</button>
+      {button}
     </li>
   )
 }
